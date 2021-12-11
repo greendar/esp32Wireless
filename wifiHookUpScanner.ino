e9783b9fcf682ed1c88ef4cfdc6903854eb82f16
Mr Green <dwgreen@hwdsb.on.ca>
2021-12-11 08:50:40 -0500

    Create wifiHookUpScanner.ino
    initial commit

---
+++ wifiHookUpScanner.ino
@@ -0,0 +1,36 @@
+#include "WiFi.h"
+
+const char* ssid = "************"; // change these
+const char* password = "***********";
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  Serial.println("Setup done");
+}
+
+void loop() {
+  Serial.println("scan start");
+  int n = WiFi.scanNetworks(); // number of networks found
+  Serial.println("scan done");
+  if (n == 0) {
+      Serial.println("no networks found");
+  } else {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i) {
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+      delay(10);
+    }
+  }
+  Serial.println("");
+  delay(5000);
+}
